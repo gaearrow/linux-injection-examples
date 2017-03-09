d34e038d0d15b8dee77bbb788d8f65e1a3de5ccc
gaearrow <gaearrow@gmail.com>
2017-03-09 10:43:38 +0800

    Update readmemx64
    Add Commit
    Change Version Info.
    Change Naming method

--- 03readmemx64/readmemx64.c
+++ 03readmemx64/readmemx64.c
@@ -1,10 +1,11 @@
 /********************************
-**filename:readmemx64.c
-**OS:CentOS 5.3 x86_64
-**GCC:version 4.1.2 20080704 (Red Hat 4.1.2-44)
-**gcc -o readmemx64 readmemx64.c
-**ps -aux | grep counter
-**./readmemx64 <pid> <start address in hex formats> <length>
+* Filename	: readmemx64.c
+* Author	: Gaearrow
+* Version	: 1.0
+* Abstract	: Read the specified memory space of the specified process
+* Platform	: gcc 4.1.2 based on CentOS 5.3 x86_64(20170308)
+* Compiling	: gcc -o readmemx64 readmemx64.c
+* Usage		: ./readmemx64 <pid> <start address in hex formats> <length>
 ********************************/
 
 #include <sys/ptrace.h>
@@ -17,93 +18,94 @@
 
 #define uchar unsigned char
 #define ulong unsigned long
-#define MAX_SIZE_READ 1048576	 	// 1 MB = 1024*1024 bytes
+#define MAX_SIZE_READ 1048576	 		// 1 MB = 1024*1024 bytes
+const int LONG_SIZE = sizeof(long);		// i386:4	x86_64:8
 
-const int long_size = sizeof(long);	// i386:4 x86_64:8
-int readmem(pid_t child, ulong saddr, char *str, int len);
+ulong readMem(pid_t pid, ulong ulSAddr, char *szMem, ulong ulLen);
 
 int main(int argc, char *argv[])
 { 
-    pid_t pid;
-    ulong saddr;
-    int len;
+	pid_t pid;
+	ulong ulSAddr;
+	int ulLen;
  
-    // Process Input
-    if(argc != 4) {
-        printf("useage: ./readmemx64 <pid> <start address(eg:2ad316875000)> "
+	/* Process Input */
+	if(argc != 4) {
+		printf("usage: ./readmemx64 <pid> <start address(eg:2ad316875000)> "
 					"<length(max:1048576 bytes)> \n");
-        return -1;
-    }
-    pid = atoi(argv[1]);
-    sscanf(argv[2],"%lx",&saddr);
-    len = atoi(argv[3]);
+		return -1;
+	}
+	pid = atoi(argv[1]);
+	sscanf(argv[2],"%lx",&ulSAddr);
+	ulLen = atoi(argv[3]);
 
-	// Reading Length Check
-	if (len > MAX_SIZE_READ){
+	/* Reading Length Check */
+	if (ulLen > MAX_SIZE_READ){
 		printf("ERROR:Length is overflow! \n");
 		return -1;
 	}
-	uchar *mem = (uchar*)malloc(len+1);	
-	if (mem == NULL) {
+
+	/* Read memory */	
+	uchar *szMem = (uchar*)malloc(ulLen+1);	
+	if (szMem == NULL) {
 		printf("ERROR:Alloc memory unsuccessfully! \n");
 		return -1;
 	}
-
-    // Read&Output memory;	
-	int rst = readmem(pid, saddr, mem, len);
-	if ( rst < 0){
+	if (readMem(pid, ulSAddr, szMem, ulLen) < 0){
 		printf("ERROR:Read memeory unsuccessfully! \n");
 		return -1;
 	}
-    
-    int i;
-    for (i = 0; i < len; i++){
-        if (i % 16 == 0) printf("\n");
-        printf("%02X ",mem[i]);
-    }
-    printf("\n");
+
+	/* Output memory */
+	int i;
+	for (i = 0; i < ulLen; i++){
+		if (i % 16 == 0) printf("\n");
+		printf("%02X ",szMem[i]);
+	}
+	printf("\n");
 	
-	free(mem);
-    return 0;
+	free(szMem);
+	return 0;
 }
 
-
-int readmem(pid_t pid, ulong saddr, char *mem, int len)
+ulong readMem(pid_t pid, ulong ulSAddr, char *szMem, ulong ulLen)
 {
-    int i = 0;
-	int j = len / long_size;
-    char *pmem = mem;   
-    union u{
-        long val;
-        char chars[long_size];
-    }data;
+	int i = 0;
+	int j = ulLen / LONG_SIZE;
+	char *pMem = szMem;   
+	union u{
+		long val;
+		char chars[LONG_SIZE];
+	}data;
 
-    // Attach to the process
-    ptrace(PTRACE_ATTACH, pid, NULL, NULL);
-    if (pid != wait(NULL)){
-        printf("Attach unsuccessfully!\n");
+	/* Attach to the process */
+	ptrace(PTRACE_ATTACH, pid, NULL, NULL);
+	if (pid != wait(NULL))
+	{
+		printf("Attach unsuccessfully!\n");
 		return -1;
-	}else{
-		printf("Attach to the process specified in pid %u Successfully!\n",pid);
-		printf("%d bytes data at 0x%lx is follow.",len,saddr);
+	}else
+	{
+		printf("Attach to the specified process pid %u successfully!\n",pid); 	// Info.
 	}
 
-    while(i < j){
-        data.val = ptrace(PTRACE_PEEKDATA, pid, saddr + i*long_size, NULL);
-        memcpy(pmem, data.chars, long_size);
-        ++i;
-        pmem += long_size;
-    }
-    j = len % long_size;
-    if(j != 0){
-        data.val = ptrace(PTRACE_PEEKDATA, pid, saddr + i*long_size, NULL);
-        memcpy(pmem, data.chars, j);
-		pmem += j;
-    }
-    pmem[0] = ' ';
+	while (i < j)
+	{
+		data.val = ptrace(PTRACE_PEEKDATA, pid, ulSAddr + i*LONG_SIZE, NULL);
+		memcpy(pMem, data.chars, LONG_SIZE);
+		++i;
+		pMem += LONG_SIZE;
+	}
+	j = ulLen % LONG_SIZE;
+	if (j != 0)
+	{
+		data.val = ptrace(PTRACE_PEEKDATA, pid, ulSAddr + i*LONG_SIZE, NULL);
+		memcpy(pMem, data.chars, j);
+		pMem += j;
+	}
 
-    // Recover the Process
-    ptrace(PTRACE_DETACH, pid, NULL, NULL);
-
-	return pmem-mem;
+	/* Recover the Process */
+	ptrace(PTRACE_DETACH, pid, NULL, NULL);
+	printf("Read %d bytes data at 0x%lx \n",pMem-szMem,ulSAddr);
+	return pMem-szMem;
 }
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Linux-Injection-Examples
-The examples of Linux Injection, Reading&amp;Writing REG/MEM and so on.
+The examples of Linux Injection, Reading REG/MEM and so on.
 
-01counter: The process to be injected
+counter		: The process to be injected
 
-./counter
+> ./counter
 
-02readripx64: Read the rip of process specified in pid
+readripx64	: Read the rip of process specified in pid
 
-./readripx64 pid
+> ./readripx64 pid
 
-03readmemx64: Read the data of process specified in pid and the specified address
+readmemx64	: Read the specified memory space of the specified process
 
-./readmemx64 pid startaddress(eg:2ad316875000) length(max:1024000 Bytes)
+> ./readmemx64 pid startaddress(eg:2ad316875000) length(max:1048576 Bytes)
